aa94c8fa1e3083c37dd11c79832acfb6530743c5
angel-torres <alas4224@gmail.com>
2019-11-13 01:11:34 -0600

    updated title for root get request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => (
-    res.send("hello world")
+    res.send("<h1>Welcome to Art Portfolio API</h1>")
 ))
 
 app.listen(port, () => (
