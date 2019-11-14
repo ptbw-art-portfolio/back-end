219d994bde4efd900f0ab44cbeb7d1867763882d
angel-torres <alas4224@gmail.com>
2019-11-14 10:10:14 -0600

    made migration file for users

--- data/migrations/20191114095323_users.js
+++ data/migrations/20191114095323_users.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('user', table => {
+      table.increments();
+      table.string('fullname', 255).notNullable();
+      table.string('username', 255).unique().notNullable();
+      table.string('email', 255).unique().notNullable();
+      table.string('password').notNullable();
+      table.timestamps(true, true);
+    });
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('user');
+  };
