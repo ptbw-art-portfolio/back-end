ca7e84aae2e79cb1a49fa1f66739cb056e49074a
angel-torres <alas4224@gmail.com>
2019-11-14 16:22:53 -0600

    moved logic for auth route to routes folder; also added comments in app.js

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+/*
+* Dependencies 
+*/
+
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const app = express();
+
+/*
+* Routes 
+*/
 
 const authRoute = require('./routes/auth')
+
+/*
+* App 
+*/
+
+const app = express();
+
+/*
+* Middleware 
+*/
 
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
-
 app.use('/auth', authRoute)
 
+/*
+* Export 
+*/
 module.exports = app;
