7ca02c7b9370397f30ffff37536a1392b4812104
angel-torres <alas4224@gmail.com>
2019-11-14 16:15:38 -0600

    added cors, helmet, and express.json middleware; also added authRoute as middleware

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
+
+const authRoute = require('./routes/auth')
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+
+app.use('/auth', authRoute)
 
 module.exports = app;
