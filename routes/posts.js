db295fca5527e9ed3e8e081f12a81a82722875e5
angel-torres <alas4224@gmail.com>
2019-11-14 20:14:04 -0600

    added endpoint to post

--- routes/posts.js
+++ routes/posts.js
@@ -20,7 +20,6 @@ postsRoute.get('/:id', (req, res) => {
     const id = req.params.id
     db('post').where({"id": id})
     .then((post) => {
-        console.log(post)
         if(post.length > 0) {
             res.status(200).json({post: post})
         } else {
@@ -33,4 +32,15 @@ postsRoute.get('/:id', (req, res) => {
     })
 })
 
+postsRoute.post('/', (req, res) => {
+    const post = req.body
+    db('post').insert(post)
+    .then((id) => {
+        res.status(201).json({post_id: id, message: "Success"})
+    })
+    .catch((err) => {
+        console.error(err)
+        res.status(500).json({message: "Internal server error"})
+    })
+})
 module.exports = postsRoute;
