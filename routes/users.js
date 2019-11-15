3ea39d5e0aa2067f9ab14c8c5775d8782087cdca
angel-torres <alas4224@gmail.com>
2019-11-14 19:35:28 -0600

    added endpoint to get one user

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,17 @@ usersRoute.get('/', (req, res) => {
     })
 });
 
+usersRoute.get('/:id', (req, res) => {
+    const id = req.params.id
+    db('user').where({"id": id})
+    .then((user) => {
+        delete user.password
+        res.status(200).json({user: user})
+    })
+    .catch((err) => {
+        console.error(err)
+        res.status(500).json({message: "Internal server error"})
+    })
+})
+
 module.exports = usersRoute;
